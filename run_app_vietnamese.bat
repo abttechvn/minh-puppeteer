73fa4a217b14da1d6a19bd552e94ca478f36b5fb
bui hieu minh <thanhniende@gmail.com>
2025-09-10 17:42:20 +0700

    feat: deleted abudance option beside download picture only

--- run_app_vietnamese.bat
+++ run_app_vietnamese.bat
@@ -9,14 +9,10 @@ echo   Bộ Tự Động Douyin - Tải Ảnh (Windows)
 echo ==============================================
 echo.
 echo  1) Tải ảnh từ ghi chú (chọn file notes_links.txt)
-echo  2) Trích xuất liên kết từ file (chọn file bất kỳ)
-echo  3) Chạy cài đặt (chỉ lần đầu)
 echo  0) Thoát
 echo.
-set /p choice=Chọn tùy chọn [0-3]: 
+set /p choice=Chọn tùy chọn [0-1]: 
 if "%choice%"=="1" goto images
-if "%choice%"=="2" goto extract
-if "%choice%"=="3" goto setup
 if "%choice%"=="0" goto end
 
 echo Lựa chọn không hợp lệ.
@@ -29,24 +25,6 @@ echo Nhập đường dẫn đến file notes_links.txt (kéo thả file vào đ
 set /p NOTES_FILE=
 if "%NOTES_FILE%"=="" goto menu
 node picture.js "%NOTES_FILE%"
-echo.
-pause
-goto menu
-
-:extract
-cls
-echo Nhập đường dẫn đến file cần trích xuất liên kết (kéo thả file vào đây) và nhấn Enter:
-set /p EXTRACT_FILE=
-if "%EXTRACT_FILE%"=="" goto menu
-node extract_links.js "%EXTRACT_FILE%"
-echo.
-pause
-goto menu
-
-:setup
-cls
-call npm install
-node setup.js
 echo.
 pause
 goto menu
